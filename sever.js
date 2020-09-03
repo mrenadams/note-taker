d960b51635556390fc64a2969d703d747334adea
mrenadams <marissaa@mendwell.com>
2020-09-02 18:04:35 -0700

    note taker hw

---
+++ sever.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const apiRoutes = require("./route/apiRoutes");
+const htmlRoutes = require("./route/htmlRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
